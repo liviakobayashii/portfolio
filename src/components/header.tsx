9f14d28196183c5d104026d11d75f676ce181e1a
liviakobayashii <liviakmm10@gmail.com>
2025-04-05 11:46:10 -0300

    update: estilização do header e da seção principal

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,11 @@
 export default function Header() {
   return (
-    <div className="flex justify-between items-center px-4 py-10 bg-black/80">
+    <div className="flex justify-between items-center px-4 py-10 bg-black/30">
       <h1 className="text-3xl font-bold">
-        Livia <span className="text-fuchsia-600">Kobayashi</span>
+        Livia
+        <span className="bg-gradient-to-r from-fuchsia-300 via-fuchsia-400 to-fuchsia-600 bg-clip-text text-transparent">
+          Kobayashi
+        </span>
       </h1>
       <button className="bg-fuchsia-600 p-3 rounded-sm text-xl">
         Baixar CV
--- src/components/main-section.tsx
+++ src/components/main-section.tsx
@@ -2,23 +2,25 @@ import Header from "./header";
 
 export default function MainSection() {
   return (
-    <section className="flex flex-col bg-[url(/background.jpg)] bg-cover h-screen">
+    <section className="flex flex-col h-screen bg-radial from-fuchsia-800 via-fuchsia-900 via-10%  to-black">
       <Header />
-      <div className="flex flex-col gap-10 justify-center items-center h-full  max-w-5xl text-center self-center">
+      <div className="flex flex-col gap-10 justify-center items-center h-full  max-w-5xl text-center self-center ">
         <h2 className="text-3xl font-bold">
-          {" "}
           Seja bem vindo(a) ao meu{" "}
-          <span className="text-fuchsia-600">portfólio</span>!
+          <span className="bg-gradient-to-r from-fuchsia-300 via-fuchsia-400 to-fuchsia-600 bg-clip-text text-transparent">
+            portfólio
+          </span>
+          !
         </h2>
         <h1 className="text-7xl font-bold self-center">
           Transformando suas ideias em resultados
         </h1>
-        <p className="text-xl text-justify text-gray-400 w-3xl">
+        {/* <p className="text-xl text-justify text-gray-400 w-3xl">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum,
           similique libero. Cumque omnis, cum error aliquam aliquid iure quo
           sequi, ipsa magnam asperiores doloribus nesciunt, neque consectetur
           voluptatem beatae suscipit.
-        </p>
+        </p> */}
       </div>
     </section>
   );
