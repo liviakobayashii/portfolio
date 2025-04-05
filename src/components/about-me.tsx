4ea08f43e75e90542f8328b611578effe179e7ad
liviakobayashii <liviakmm10@gmail.com>
2025-04-05 12:16:42 -0300

    update: estilização da seção sobre mim

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -2,23 +2,22 @@ import Social from "./icons/social";
 
 export default function AboutMe() {
   return (
-    <section className="flex flex-col gap-6 justify-center items-center my-30">
+    <section className="flex flex-col gap-6 justify-center items-center my-30 h-screen bg-radial from-[#242424] via-[#1c1c1c] to-[#161616]">
       <div className="flex flex-col gap-4 w-6xl">
-        <img src="perfil.jpg" alt="Minha foto" className="w-80 rounded-sm" />
-        <h2 className="text-4xl text-fuchsia-600">
-          Livia Kobayashi Machado Martins
+        <img src="perfil.jpg" alt="Minha foto" className="w-70 rounded-sm" />
+        <h2 className="text-4xl bg-gradient-to-r from-fuchsia-300 via-fuchsia-500 to-fuchsia-800 bg-clip-text text-transparent font-bold">
+          Front-end developer | UI/UX Designer
         </h2>
         <hr className="text-fuchsia-600" />
       </div>
-      <div className="flex flex-col gap-4 w-6xl">
-        <p className="text-justify text-gray-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          repellendus nisi pariatur facere, impedit sequi expedita aut corporis
-          earum explicabo hic? Quis ad aperiam beatae quod dicta totam molestias
-          a? Sint mollitia porro labore, deserunt, perferendis itaque doloribus
-          cum perspiciatis quod facere quaerat incidunt sunt tenetur fugit eius,
-          odio dolore quam rem voluptatibus! Repellat adipisci perspiciatis quia
-          officiis voluptatem! Possimus.
+      <div className="flex flex-col gap-6 w-6xl">
+        <p className="text-justify text-fuchsia-200 text-xl">
+          Desenvolvedora Front-end & UI/UX Designer, movida por desafios e
+          apaixonada por tecnologia. Tenho um olhar atento para a experiência do
+          usuário e foco em criar interfaces funcionais, bem estruturadas e
+          visualmente atrativas. Estou em constante evolução, sempre buscando
+          aprender, crescer e transformar boas ideias em experiências digitais
+          incríveis.
         </p>
         <Social />
       </div>
