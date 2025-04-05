1baf09f80ace6be72b77a2be09025bfb147f0e4e
liviakobayashii <liviakmm10@gmail.com>
2025-04-05 11:23:17 -0300

    update: link para as redes sociais

--- src/components/icons/social-icons.tsx
+++ src/components/icons/social-icons.tsx
@@ -1,13 +1,17 @@
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 type Props = {
+  href: string;
   icon: string;
 };
 
-export default function SocialIcons({ icon }: Props) {
+export default function SocialIcons({ href, icon }: Props) {
   return (
-    <button className="bg-fuchsia-600 p-3 rounded-sm">
-      <Icon icon={icon} className="size-5" />
-    </button>
+    <Link href={href} target="_blank">
+      <button className="bg-fuchsia-600 p-3 rounded-sm cursor-pointer hover:bg-fuchsia-700 duration-200">
+        <Icon icon={icon} className="size-5" />
+      </button>
+    </Link>
   );
 }
--- src/components/icons/social.tsx
+++ src/components/icons/social.tsx
@@ -3,11 +3,23 @@ import SocialIcons from "./social-icons";
 export default function Social() {
   return (
     <div className="flex gap-3">
-      <SocialIcons icon="mdi:facebook" />
-      <SocialIcons icon="mdi:instagram" />
-      <SocialIcons icon="mdi:whatsapp" />
-      <SocialIcons icon="mdi:github" />
-      <SocialIcons icon="mdi:linkedin" />
+      <SocialIcons
+        href="https://www.linkedin.com/in/liviakobayashii"
+        icon="mdi:linkedin"
+      />
+      <SocialIcons
+        href="https://github.com/liviakobayashii"
+        icon="mdi:github"
+      />
+      <SocialIcons href="https://wa.me/5514920006436" icon="mdi:whatsapp" />
+      <SocialIcons
+        href="https://www.facebook.com/liviakobayashii"
+        icon="mdi:facebook"
+      />
+      <SocialIcons
+        href="https://www.instagram.com/liviakobayashii"
+        icon="mdi:instagram"
+      />
     </div>
   );
 }
