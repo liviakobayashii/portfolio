7c8164c1922a84057ccc48c0a31435835ab4b58c
liviakobayashii <liviakmm10@gmail.com>
2025-04-11 14:14:36 -0300

    update: logo na página e no favicon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,13 +2,16 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex max-md:flex-col md:justify-between md:items-center px-4 py-10 bg-black/30">
-      <h1 className="text-3xl font-bold text-neutral-200">
-        Livia
-        <span className="bg-gradient-to-r from-fuchsia-300 via-fuchsia-400 to-fuchsia-600 bg-clip-text text-transparent">
-          Kobayashi
-        </span>
-      </h1>
+    <div className="flex max-md:flex-col md:justify-between md:items-center px-3 py-5 bg-black/30">
+      <div className="flex justify-center items-center">
+        <img src="logo.png" alt="Logo" className="size-30" />
+        <h1 className="text-3xl font-bold text-neutral-200">
+          Livia
+          <span className="bg-gradient-to-r from-fuchsia-300 via-fuchsia-400 to-fuchsia-600 bg-clip-text text-transparent">
+            Kobayashi
+          </span>
+        </h1>
+      </div>
       <Link href="curriculo.pdf" target="_blank">
         <button className=" max-md:hidden bg-fuchsia-600 p-3 rounded-sm text-neutral-200 font-bold hover:cursor-pointer hover:bg-fuchsia-700 duration-200">
           Baixar CV
