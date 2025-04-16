954382e61ec57935b99ae5a6763227cf125686bb
liviakobayashii <liviakmm10@gmail.com>
2025-04-16 11:13:33 -0300

    update: estilização dos h2

--- src/components/h2.tsx
+++ src/components/h2.tsx
@@ -1,6 +1,6 @@
 export default function H2({ text }: { text: string }) {
   return (
-    <h2 className="max-sm:text-3xl sm:text-4xl lg:text-5xl bg-gradient-to-r from-fuchsia-300 via-fuchsia-500 to-fuchsia-800 bg-clip-text text-transparent font-bold">
+    <h2 className="max-sm:text-3xl sm:text-4xl lg:text-5xl bg-gradient-to-r from-fuchsia-300 via-fuchsia-500 to-fuchsia-800 bg-clip-text text-transparent font-bold pb-2">
       {text}
     </h2>
   );
