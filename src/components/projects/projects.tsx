1a374df236deb699def81cd567c16feae5de6cd9
liviakobayashii <liviakmm10@gmail.com>
2025-04-16 11:12:29 -0300

    update: exibição dos projetos

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -71,7 +71,7 @@ export default function Projects() {
       </ul>
 
       <div className="grid max-sm:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-4 p-10">
-        {filteredProjects.map((item) => {
+        {filteredProjects.reverse().map((item) => {
           const badges = technologies.filter((tec) =>
             item.project_technologies.includes(tec.name)
           );
