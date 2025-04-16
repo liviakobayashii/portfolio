cb5320545a3eeeeef88e1c372908b1f1a8d9623b
liviakobayashii <liviakmm10@gmail.com>
2025-04-16 11:25:07 -0300

    update: estilização do header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,29 @@
+"use client";
+
 import Link from "next/link";
 import Li from "./li";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 200) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    };
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
   return (
-    <div className="flex max-md:flex-col md:justify-between md:items-center px-3 bg-black/30">
+    <div
+      className={`flex max-md:flex-col md:justify-between md:items-center fixed w-screen z-53 px-10 duration-300 ${
+        visible ? "bg-black/90" : "bg-black/30"
+      }`}
+    >
       <div className="flex justify-center items-center">
         <img src="logo.png" alt="Logo" className="size-30" />
         <h1 className="text-3xl font-bold text-neutral-200">
